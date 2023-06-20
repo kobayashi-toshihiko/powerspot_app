2610600778990925749cb2b5c9cc2aefe37e124a
kobayashi-toshihiko <toshihiko316@gmail.com>
2023-06-20 20:42:43 +0900

    アソシエーションバリデーション設定

--- app/models/category_blessing.rb
+++ app/models/category_blessing.rb
@@ -1,2 +1,4 @@
 class CategoryBlessing < ApplicationRecord
+  has_many :power_spots
+  validates :name, presence: true
 end
--- app/models/power_spot.rb
+++ app/models/power_spot.rb
@@ -1,2 +1,8 @@
 class PowerSpot < ApplicationRecord
+  belongs_to :user
+  belongs_to :attribute
+  belongs_to :category_blessing
+  belongs_to :prefecture
+  has_many :comments
+  validates :name, :address, :descript, :kami, :image1, presence: true
 end
