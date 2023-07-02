828c169d3a2b4fb6f75411c5d03b630d84375f73
kobayashi-toshihiko <toshihiko316@gmail.com>
2023-07-02 19:11:01 +0900

    ストロングパラメータに追加と修正

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,8 @@
 class ApplicationController < ActionController::Base
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+  def configure_permitted_parameters
+    #ストロングパラメータに「:birthday, :prefecture_id, :blood_type, :profile, :avatar」の追加
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:username, :birthday, :prefecture_id, :blood_type, :profile, :avatar])
+  end
 end
