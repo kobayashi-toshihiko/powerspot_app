96ee28eedac51ceb9425f664a1790fc7c80d7e8a
kobayashi-toshihiko <toshihiko316@gmail.com>
2023-07-03 00:02:21 +0900

    top画面作成

--- app/controllers/power_spots_controller.rb
+++ app/controllers/power_spots_controller.rb
@@ -1,4 +1,9 @@
 class PowerSpotsController < ApplicationController
   def index
+    youngest = 5
+    oldest = 85
+    @youngest_year = (Time.now.year) - youngest
+    @oldest_year = (Time.now.year) - oldest
   end
+
 end
