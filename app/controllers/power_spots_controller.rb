6620d620ea7a6d4b57ded57270397a89e276d523
kobayashi-toshihiko <toshihiko316@gmail.com>
2023-07-04 01:07:16 +0900

    @oldest_year..@youngest_yearをリテラルで修正

--- app/controllers/power_spots_controller.rb
+++ app/controllers/power_spots_controller.rb
@@ -1,9 +1,5 @@
 class PowerSpotsController < ApplicationController
   def index
-    youngest = 5
-    oldest = 85
-    @youngest_year = (Time.now.year) - youngest
-    @oldest_year = (Time.now.year) - oldest
   end
 
 end
