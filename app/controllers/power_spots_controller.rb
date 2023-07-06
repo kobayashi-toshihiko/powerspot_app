bb0e91a1a2cf3ba04dc5fbd7118cdfaf59f27ea1
kobayashi-toshihiko <toshihiko316@gmail.com>
2023-07-06 22:40:51 +0900

    都道府県別パワースポット一覧画面作成

--- app/controllers/power_spots_controller.rb
+++ app/controllers/power_spots_controller.rb
@@ -2,4 +2,7 @@ class PowerSpotsController < ApplicationController
   def index
   end
 
+  def prefecture_powerspot
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users
   root 'power_spots#index'
+  get 'power_spots/prefecture_powerspot', to: 'power_spots#prefecture_powerspot'
   resources :power_spots, only: [:index, :new, :create]
 end
