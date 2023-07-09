0cde6dc877713bbf9ff051432f5909dcba6daf81
kobayashi-toshihiko <toshihiko316@gmail.com>
2023-07-09 21:15:57 +0900

    都道府県一覧を修正

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,10 @@
 Rails.application.routes.draw do
   devise_for :users
   root 'power_spots#index'
-  # resources :prefectures, only: :show do
-  #   resources :powerspots, only: :index
-  # end
-
-  resources :prefectures, only: [:index]
+  resources :prefectures, only: :show do
+    resources :spots, only: :index
+  end
+  resources :prefectures, only: [:index, :show]
   resources :power_spots, only: [:index]
-  
 end
 
