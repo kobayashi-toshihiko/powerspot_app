336c953661b380bf8abdd0a623ccd76724f3be1c
kobayashi-toshihiko <toshihiko316@gmail.com>
2023-07-15 15:37:49 +0900

    first commit

--- app/controllers/prefectures/power_spots_controller.rb
+++ app/controllers/prefectures/power_spots_controller.rb
@@ -3,4 +3,10 @@ class Prefectures::PowerSpotsController < ApplicationController
     @prefecture = Prefecture.find(params[:prefecture_id])
     @power_spots = @prefecture.power_spots #戻り値は配列なので、view側ではeachで処理する。
   end
+  
+  def show
+    @prefecture = Prefecture.find(params[:prefecture_id])
+    @power_spots = @prefecture.power_spots #戻り値は配列なので、view側ではeachで処理する。
+    # @element = @power_spots[0].element
+  end
 end
